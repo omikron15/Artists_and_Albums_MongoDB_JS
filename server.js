588e76169c6deecf83adac272af0836869c9a7d6
omikron15 <omikron15@hotmail.co.uk>
2018-06-19 18:50:18 +0100

    add connection to localhost db

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const parser = require('body-parser');
 const server = express();
 
 server.use(parser.json());
-server.use(express.static('client/build'));
 server.use(parser.urlencoded({extended: true}));
+
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+
+MongoClient.connect("mongodb://localhost:27017", function(err, client){
+  if(err){
+    console.log(err);
+    return;
+  }
+})
+
+
 
 server.listen(3000, function(){
   console.log("Listening on port 3000");
